0eb569cae0b00fd0a39721281d205b64a544019d
Sourav Bagchi <souravbagchi9939@gmail.com>
2026-06-29 17:53:24 +0530

    Simplify settings modal open and close actions
    Refactor settings modal display logic to use CSS classes.

--- js/citizen.js
+++ js/citizen.js
@@ -786,17 +786,11 @@ const JanVikasCitizen = {
     settingsBtn.addEventListener('click', (e) => {
       e.preventDefault();
       updateStatusDisplay();
-      settingsModal.style.display = 'flex';
-      setTimeout(() => {
-        settingsModal.style.opacity = '1';
-      }, 10);
+      settingsModal.classList.add('open');
     });
 
     const closeModal = () => {
-      settingsModal.style.opacity = '0';
-      setTimeout(() => {
-        settingsModal.style.display = 'none';
-      }, 200);
+      settingsModal.classList.remove('open');
     };
 
     if (settingsCloseBtn) {
